0a34ae3c5d8936c9c9132f6738dc558adb5e2e79
KYapp <kukiyomenaisapp@gmail.com>
2022-07-14 21:51:01 +0900

    Init

---
+++ Jlib/Jlib.cs
@@ -0,0 +1,16 @@
+﻿
+namespace JuhaKurisu.Jlib
+{
+    public static class Jlib
+    {
+        public static char TryGet(this string str,int c)
+        {
+            if (str.Length - 1 > c)
+            {
+                return str[c];
+            }
+
+            return '\0';
+        }
+    }
+}
