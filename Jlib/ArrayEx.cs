2d859b778f04eaf48f3c3c5ff69ca3f1207777ed
KYapp <kukiyomenaisapp@gmail.com>
2022-07-14 22:09:12 +0900

    名前変更とArrayのtrygetvalueを実装

---
+++ Jlib/ArrayEx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuhaKurisu.Jlib
+{
+    public static partial class Jlib
+    {
+        public static bool TryGetValue<T>(this T[] array, int index, out T value)
+        {
+            value = default;
+
+            // マイナスの場合false
+            if (index < 0) return false;
+
+            // 範囲外の場合false
+            if (array.Length - 1 < index) return false;
+
+            value = array[index];
+            return true;
+        }
+    }
+}
---
+++ Jlib/StringEx.cs
@@ -0,0 +1,21 @@
+﻿
+namespace JuhaKurisu.Jlib
+{
+    public static partial class Jlib
+    {
+        /// <summary>
+        /// 与えられた位置に文字があった場合はそれを返し、なかった場合はヌル文字を返す
+        /// </summary>
+        /// <param name="c">位置</param>
+        /// <returns>取得した文字</returns>
+        public static char TryGet(this string str,int c)
+        {
+            if (str.Length - 1 > c)
+            {
+                return str[c];
+            }
+
+            return '\0';
+        }
+    }
+}
