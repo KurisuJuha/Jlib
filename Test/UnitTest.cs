840502a1cefe949475dc29dfda81c3a0163e6354
KYapp <kukiyomenaisapp@gmail.com>
2022-07-17 15:01:22 +0900

    UnitTestの追加

---
+++ Test/UnitTest.cs
@@ -0,0 +1,23 @@
+﻿namespace Test;
+
+public class Test
+{
+    [Fact]
+    public void ArrayEx_TryGetValue()
+    {
+        int[] test = new int[]
+        {
+            10,
+            20,
+            30,
+            40,
+        };
+
+        if (test.TryGetValue(0 ,out int value))
+        {
+            Assert.Equal(10, value);
+        }
+
+        Assert.True(!test.TryGetValue(5, out int _value));
+    }
+}
